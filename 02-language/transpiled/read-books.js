e131045d94d656dd1cf70446d5fb238b6b908bdf
lurumad <luisruizpavon@gmail.com>
2024-11-29 18:23:19 +0100

    wip: remove transpiled

--- 02-language/transpiled/read-books.js
+++
@@ -1,8 +0,0 @@
-export var isBookRead = function (books, title) {
-    return (books.find(function (book) { return titlesMatch(book.title, title) && book.isRead; }) !==
-        undefined);
-};
-var titlesMatch = function (bookTitle, searchTitle) {
-    return bookTitle.localeCompare(searchTitle, undefined, { sensitivity: "base" }) ===
-        0;
-};
